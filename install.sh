69d32a834233b362ba94a8a855be5fecdb8a0523
Marian Minar <majamin@gmail.com>
2022-09-21 20:41:23 -0700

    something

--- install.sh
+++ install.sh
@@ -14,20 +14,20 @@ timedatectl set-ntp true
 # Partitions
 parted -s -a optimal "$DEVICE" -- mklabel gpt
 parted -s -a optimal "$DEVICE" mkpart primary 1MiB 513MiB
-parted -s -a optimal "$DEVICE" mkpart primary 514MiB "(( $RAM * 2048 ))"MiB
+parted -s -a optimal "$DEVICE" mkpart primary 514MiB "$(($RAM * 2048))"MiB
 parted -s -a optimal "$DEVICE" mkpart primary "(( $RAM * 2048 + 1 ))"MiB 40%
 parted -s -a optimal "$DEVICE" mkpart primary 40% 100%
 parted "$DEVICE" set 1 esp on
 
 # format
-mkfs.fat -F32 "$DEVICE"/1
-mkswap "$DEVICE"/2
+mkfs.fat -F32 $BOOT
+mkswap $SWAP
 swapon
-mkfs.ext4 "$DEVICE"/3
-mkfs.ext4 "$DEVICE"/4
+mkfs.ext4 $ROOT
+mkfs.ext4 $HOME
 
 # get fast mirrors
-curl -s "https://archlinux.org/mirrorlist/?country=${REGION}&protocol=https&use_mirror_status=on" | sed - e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist && pacman -Syy
+#curl -s "https://archlinux.org/mirrorlist/?country=${REGION}&protocol=https&use_mirror_status=on" | sed - e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist && pacman -Syy
 
 # mount partitions
 mount $ROOT /mnt
